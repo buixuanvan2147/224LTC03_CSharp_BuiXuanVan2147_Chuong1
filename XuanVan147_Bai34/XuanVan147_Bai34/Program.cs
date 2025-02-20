88e985107ad917033461b77d107271a7cb2c403c
buixuanvan2147 <22115053122147@sv.ute.udn.vn>
2025-02-20 23:48:39 +0700

    Create branch Bai34 and push blank project containing only student information

---
+++ XuanVan147_Bai34/XuanVan147_Bai34/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XuanVan147_Bai34
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Xuất chữ tiếng Việt
+            Console.OutputEncoding = Encoding.UTF8;
+
+            // Thông tin sinh viên
+            Console.WriteLine("Họ tên: Bùi Xuân Văn \nMsv: 22115053122147 \nLớp học phần: 224LTC03 (C# thứ 7 tiết 3-4)");
+            //Đề bài
+            Console.WriteLine("--------------\nBài 34: Tính S(n) = CanBac2(n+CanBac2(n – 1 + CanBac2( n – 2 + … + CanBac2(2 + CanBac2(1)  có n dấu căn");
+        }
+    }
+}
---
+++ XuanVan147_Bai34/XuanVan147_Bai34/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("XuanVan147_Bai34")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("XuanVan147_Bai34")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("129ef3c9-cd5b-45f0-a12d-be637f96bc8a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
