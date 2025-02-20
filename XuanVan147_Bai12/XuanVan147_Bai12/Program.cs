49245eb3c37c724fb61fefc6bca9d2be75a33339
buixuanvan2147 <22115053122147@sv.ute.udn.vn>
2025-02-20 22:17:32 +0700

    add project

---
+++ XuanVan147_Bai12/XuanVan147_Bai12/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XuanVan147_Bai12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Tính S(n) = x + x^2 + x^3 + … + x^n
+
+            //Xuất chữ tiếng Việt
+            Console.OutputEncoding = Encoding.UTF8;
+        }
+    }
+}
