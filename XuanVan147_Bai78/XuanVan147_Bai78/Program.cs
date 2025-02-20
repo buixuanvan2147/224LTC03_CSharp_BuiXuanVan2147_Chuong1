917bca5d41dd006385657d315899110f3e58bfb7
buixuanvan2147 <22115053122147@sv.ute.udn.vn>
2025-02-21 02:03:48 +0700

    add student information and assignment name

--- XuanVan147_Bai78/XuanVan147_Bai78/Program.cs
+++ XuanVan147_Bai78/XuanVan147_Bai78/Program.cs
@@ -10,6 +10,13 @@ namespace XuanVan147_Bai78
     {
         static void Main(string[] args)
         {
+            //Xuất chữ tiếng Việt
+            Console.OutputEncoding = Encoding.UTF8;
+
+            // Thông tin sinh viên
+            Console.WriteLine("Họ tên: Bùi Xuân Văn \nMsv: 22115053122147 \nLớp học phần: 224LTC03 (C# thứ 7 tiết 3-4)");
+            //Đề bài
+            Console.WriteLine("--------------\nBài 78: Liệt kê tất cả các ước số của số nguyên dương n \n--------------");
         }
     }
 }
