0550c122ba48e10a9b930b6aac1898d84f6b4fdc
buixuanvan2147 <22115053122147@sv.ute.udn.vn>
2025-02-21 01:23:33 +0700

    fix logic code and console.writeline

--- XuanVan147_Bai56/XuanVan147_Bai56/Program.cs
+++ XuanVan147_Bai56/XuanVan147_Bai56/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -19,6 +20,7 @@ namespace XuanVan147_Bai56
             Console.WriteLine("--------------\nBài 56: Hãy kiểm tra số nguyên dương n có toàn chữ số lẻ hay không \n--------------");
 
             int n_147;
+            int tongCacChuSo_147 = 0;
             List<int> listChuSoLe = new List<int>();
 
             Console.Write("Nhập n = ");
@@ -35,23 +37,31 @@ namespace XuanVan147_Bai56
             //Kiem tra
             while (temp_147 > 0)
             {
+                tongCacChuSo_147++;
                 int chuSo_147 = temp_147 % 10;
                 if (chuSo_147 % 2 != 0)
                 {
-                    listChuSoLe.Add(chuSo_147);
+                    listChuSoLe.Add(chuSo_147);//thêm chữ số lẻ vào list
                 }
                 temp_147 = temp_147 / 10;
             }
 
             //Xuất kết quả
-            int dem = listChuSoLe.Count;
-            if (dem == 0)
+            int dem_147 = listChuSoLe.Count;// đếm các chữ số lẻ
+            if (dem_147 == 0)
             {
                 Console.WriteLine("Số {0} không có chữ số lẻ nào", n_147);
             }
             else
             {
-                Console.WriteLine("Số {0} có {1} chữ số lẻ là các chữ số: {2}", n_147, dem, string.Join(" ",listChuSoLe));
+                if (tongCacChuSo_147 == dem_147)
+                {
+                    Console.WriteLine("Số {0} có toàn bộ là các chữ số lẻ",n_147);
+                }
+                else
+                {
+                    Console.WriteLine("Số {0} không phải có toàn là các chữ số lẻ \n Có {1} chữ số lẻ là các chữ số: {2}", n_147, dem_147, string.Join(" ", listChuSoLe));
+                }
             }
 
             Console.ReadKey();
