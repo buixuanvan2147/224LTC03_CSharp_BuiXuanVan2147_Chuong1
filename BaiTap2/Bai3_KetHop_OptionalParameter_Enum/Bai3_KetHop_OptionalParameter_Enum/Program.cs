8c0310cf9c837d6f9fee2dbf96690b5852853be6
buixuanvan2147 <22115053122147@sv.ute.udn.vn>
2025-02-27 13:33:36 +0700

    Tạo bài tập 3 - kết hợp Optional parameter và Enum

---
+++ BaiTap2/Bai3_KetHop_OptionalParameter_Enum/Bai3_KetHop_OptionalParameter_Enum/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai3_KetHop_OptionalParameter_Enum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
