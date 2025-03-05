76204e6ca8d6732363690cd0884445352b141b9e
buixuanvan2147 <22115053122147@sv.ute.udn.vn>
2025-03-05 23:07:27 +0700

    Viết Abstract class Employee_147 kế thừa Interface IEmployee_147, lưu thông tin chung của nhân viên

--- BuiXuanVan147_BaiTapOOP/BuiXuanVan147_BaiTapOOP/Program.cs
+++ BuiXuanVan147_BaiTapOOP/BuiXuanVan147_BaiTapOOP/Program.cs
@@ -12,6 +12,47 @@ namespace BuiXuanVan147_BaiTapOOP
         int CalculateSalary_147(); //Phương thức tính lương cho nhân viên
         string GetName_147(); //Phương thức lấy tên nhân viên
     }
+
+    //Abstract class Employee_147 kế thừa Interface IEmployee_147, lưu thông tin chung của nhân viên
+    public abstract class Employee_147 : IEmployee_147
+    {
+        protected string name_147; //Tên nhân viên
+        protected int paymentPerHour_147; //Lương làm việc trên mỗi giờ
+
+        //Khởi tạo Constructor
+        public Employee_147(string name_147, int paymentPerHour_147)
+        {
+            this.name_147 = name_147;
+            this.paymentPerHour_147 = paymentPerHour_147;
+        }
+
+        //Phương thức set tên nhân viên
+        public void SetName_147(string name_147)
+        {
+            this.name_147 = name_147;
+        }
+
+        // Phương thức get tên nhân viên
+        public string GetName_147()
+        {
+            return this.name_147;
+        }
+
+        //Phương thức set tiền lương mỗi giờ
+        public void SetPaymentPerHour_147(int paymentPerHour_147)
+        {
+            this.paymentPerHour_147 = paymentPerHour_147;
+        }
+
+        // Phương thức get tiền lương mỗi giờ
+        public int GetPaymentPerHour_147()
+        {
+            return this.paymentPerHour_147;
+        }
+
+        // Phương thức tính lương để gọi lại ở các class con
+        public abstract int CalculateSalary_147();
+    }
     internal class Program
     {
         static void Main(string[] args)
