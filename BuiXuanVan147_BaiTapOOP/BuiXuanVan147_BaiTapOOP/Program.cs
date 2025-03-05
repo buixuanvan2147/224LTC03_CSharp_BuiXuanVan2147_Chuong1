cedb8fce63e8c1a979ff88bb58f73eb46013f641
buixuanvan2147 <22115053122147@sv.ute.udn.vn>
2025-03-05 22:57:59 +0700

    Viết Interface IEmployee_147 chứa các phương thức chung cho nhân viên

--- BuiXuanVan147_BaiTapOOP/BuiXuanVan147_BaiTapOOP/Program.cs
+++ BuiXuanVan147_BaiTapOOP/BuiXuanVan147_BaiTapOOP/Program.cs
@@ -6,10 +6,19 @@ using System.Threading.Tasks;
 
 namespace BuiXuanVan147_BaiTapOOP
 {
+    //Interface IEmployee_147 chứa phương thức chung cho nhân viên
+    public interface IEmployee_147
+    {
+        int CalculateSalary_147(); //Phương thức tính lương cho nhân viên
+        string GetName_147(); //Phương thức lấy tên nhân viên
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.UTF8;//Cho phép xuất tiếng Việt
+
+
         }
     }
 }
