5aad242bf8be5bbda4c1b47d7e1c2a99cece0f13
buixuanvan2147 <22115053122147@sv.ute.udn.vn>
2025-03-06 00:12:19 +0700

    Viết main với list lưu danh sách nhân viên dùng while, switch- case

--- BuiXuanVan147_BaiTapOOP/BuiXuanVan147_BaiTapOOP/Program.cs
+++ BuiXuanVan147_BaiTapOOP/BuiXuanVan147_BaiTapOOP/Program.cs
@@ -106,7 +106,142 @@ namespace BuiXuanVan147_BaiTapOOP
         {
             Console.OutputEncoding = Encoding.UTF8;//Cho phép xuất tiếng Việt
 
+            //List lưu danh sách nhân viên
+            List<PartTimeEmployee_147> partTimeEmployees_147 = new List<PartTimeEmployee_147>();
+            List<FullTimeEmployee_147> fullTimeEmployees_147 = new List<FullTimeEmployee_147>();
 
+            int luaChon = -1;
+            while (luaChon != 0)
+            {
+                Console.WriteLine("\n===========>>>    MENU    <<<==========");
+                Console.WriteLine(" 1. Thêm nhân viên PartTime");
+                Console.WriteLine(" 2. Thêm nhân viên FullTime");
+                Console.WriteLine(" 3. Xem danh sách nhân viên PartTime");
+                Console.WriteLine(" 4. Xem danh sách nhân viên FullTime");
+                Console.WriteLine(" 5. Xem tất cả nhân viên");
+                Console.WriteLine(" 0. Thoát");
+                Console.WriteLine("=======================================");
+                Console.Write("Nhập lựa chọn của bạn: ");
+                while(!int.TryParse(Console.ReadLine(), out luaChon)  || luaChon < 0 || luaChon > 5)
+                {
+                    Console.Write("Lựa chọn không hợp lệ! Nhập lại (0-5): ");
+                };
+
+                switch (luaChon)
+                {
+                    case 0:
+                        Console.WriteLine("\nKết thúc chương trình!");
+                        break;
+                    case 1:
+                        //Thêm nhân viên PartTime
+                        Console.Write("\nNhập tên nhân viên: ");
+                        string namePT_147 = Console.ReadLine();
+
+                        Console.Write("Nhập lương mỗi giờ: ");
+                        int paymentPerHourPT_147;
+                        while (!int.TryParse(Console.ReadLine(), out paymentPerHourPT_147) || paymentPerHourPT_147 <= 0)
+                        {
+                            Console.Write("Lương phải lớn hơn 0! Nhập lại: ");
+                        }
+
+                        Console.Write("Nhập số giờ làm việc mỗi ngày: ");
+                        int workingHoursPT_147;
+                        while (!int.TryParse(Console.ReadLine(), out workingHoursPT_147) || workingHoursPT_147 <= 0)
+                        {
+                            Console.Write("Giờ làm phải lớn hơn 0! Nhập lại: ");
+                        }
+
+                        PartTimeEmployee_147 newPartTimeEmployee_147 = new PartTimeEmployee_147(namePT_147, paymentPerHourPT_147, workingHoursPT_147);
+                        partTimeEmployees_147.Add(newPartTimeEmployee_147);
+                        Console.WriteLine("Thêm nhân viên Part-Time thành công!\n");
+                        break;
+                    case 2:
+                        //Thêm nhân viên FullTime
+                        Console.Write("\nNhập tên nhân viên: ");
+                        string nameFT_147 = Console.ReadLine();
+
+                        Console.Write("Nhập lương mỗi giờ: ");
+                        int paymentPerHourFT_147;
+                        while (!int.TryParse(Console.ReadLine(), out paymentPerHourFT_147) || paymentPerHourFT_147 <= 0)
+                        {
+                            Console.Write("Lương phải lớn hơn 0! Nhập lại: ");
+                        }
+
+                        FullTimeEmployee_147 newFullTimeEmployee_147 = new FullTimeEmployee_147(nameFT_147, paymentPerHourFT_147);
+                        fullTimeEmployees_147.Add(newFullTimeEmployee_147);
+                        Console.WriteLine("Thêm nhân viên Full-Time thành công!\n");
+                        break;
+                    case 3:
+                        //Xem danh sách nhân viên PartTime
+                        if (partTimeEmployees_147.Count == 0)
+                        {
+                            Console.WriteLine("\nKhông có nhân viên Part-Time nào!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("\nDanh sách nhân viên Part-Time:");
+                            foreach (var emp in partTimeEmployees_147)
+                            {
+                                Console.WriteLine("+ Tên nhân viên: {0}\n  Tiền lương một ngày: {1} VND",emp.GetName_147(), emp.CalculateSalary_147());
+                            }
+                        }
+                        break;
+                    case 4:
+                        //Xem danh sách nhân viên FullTime
+                        if (fullTimeEmployees_147.Count == 0)
+                        {
+                            Console.WriteLine("\nKhông có nhân viên Full-Time nào!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("\nDanh sách nhân viên Full-Time:");
+                            foreach (var emp in fullTimeEmployees_147)
+                            {
+                                Console.WriteLine("+ Tên nhân viên: {0}\n  Tiền lương một ngày: {1} VND", emp.GetName_147(), emp.CalculateSalary_147());
+                            }
+                        }
+                        break;
+                    case 5:
+                        //Danh sách tất cả nhân viên
+
+                        if (partTimeEmployees_147.Count == 0 && fullTimeEmployees_147.Count == 0)
+                        {
+                            Console.WriteLine("\nDanh sách trống!");
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("\nDanh sách tất cả nhân viên:");
+                        }
+
+                        if (partTimeEmployees_147.Count == 0)
+                        {
+                            Console.WriteLine("- Không có nhân viên Part-Time nào!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("- Danh sách nhân viên Part-Time:");
+                            foreach (var emp in partTimeEmployees_147)
+                            {
+                                Console.WriteLine("   + Tên nhân viên: {0}\n     Tiền lương một ngày: {1} VND", emp.GetName_147(), emp.CalculateSalary_147());
+                            }
+                        }
+                        if (fullTimeEmployees_147.Count == 0)
+                        {
+                            Console.WriteLine("- Không có nhân viên Full-Time nào!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("- Danh sách nhân viên Full-Time:");
+                            foreach (var emp in fullTimeEmployees_147)
+                            {
+                                Console.WriteLine("   + Tên nhân viên: {0}\n     Tiền lương một ngày: {1} VND", emp.GetName_147(), emp.CalculateSalary_147());
+                            }
+                        }
+                        break;
+                }              
+            }
+            Console.ReadKey();
         }
     }
 }
